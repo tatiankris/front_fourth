f03bc07477df881171100b064f8eb16af5bde20f
tatiankris <tatiankris@mail.ru>
2022-12-01 16:27:01 +0300

    refactor

--- src/hooks.ts
+++ src/hooks.ts
@@ -7,16 +7,18 @@ export const useAppDispatch: () => AppDispatch = useDispatch
 export const useAppSelector: TypedUseSelectorHook<AppRootStateType> = useSelector
 
 //debounce for api
-export function useDebounce<T>(value: T, delay?: number): T {
-    const [debouncedValue, setDebouncedValue] = useState<T>(value)
 
-    useEffect(() => {
-        const timer = setTimeout(() => setDebouncedValue(value), delay || 500)
-
-        return () => {
-            clearTimeout(timer)
-        }
-    }, [value, delay])
-
-    return debouncedValue
-}
+//УДАЛИТЬ
+// export function useDebounce<T>(value: T, delay?: number): T {
+//     const [debouncedValue, setDebouncedValue] = useState<T>(value)
+//
+//     useEffect(() => {
+//         const timer = setTimeout(() => setDebouncedValue(value), delay || 500)
+//
+//         return () => {
+//             clearTimeout(timer)
+//         }
+//     }, [value, delay])
+//
+//     return debouncedValue
+// }
